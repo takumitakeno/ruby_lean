1f956a81cc022feb376bb4389c36d98d4c46a3d2
takumitakeno <matchball.shine@gmail.com>
2020-07-19 18:57:01 +0900

    add test.csv

--- csv/test.rb
+++ csv/test.rb
@@ -1,20 +1,30 @@
 require 'csv'
 
-# 1つめの構文で読み込む
+files = []
 
-csv = CSV.read("sample1.csv", headers: false)
+dir = Dir.open(".") 
+dir.each do |file|
+	files << file
+end
+
+file_all = files.select { |x| x.include?("csv")}
+
 array = []
-
-csv.each do |row|
-    array << row
+file_all.each_with_index do |file_name, index|
+	if index == 0
+	 	csv = CSV.read("#{file_name}", headers: false)
+		csv.each do |row|
+			array << row
+		end
+	else
+		csv = CSV.read("#{file_name}", headers: true)
+		csv.each do |row|
+			array << row
+		end
+	end
 end
 
-csv = CSV.read("sample2.csv", headers: true) 
-csv.each do |row|
- 	array << row
-end
-
-CSV.open("test.csv","w")do |test|
+CSV.open("test.csv","w") do |test|
     array.each do |data|
     	test << data
     end
