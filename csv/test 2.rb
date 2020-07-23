cc8006b1836ed470b8c0eaa454ffdc5dbc50bace
takumitakeno <matchball.shine@gmail.com>
2020-07-23 11:14:39 +0900

    add test2.rb

---
+++ csv/test 2.rb
@@ -0,0 +1,35 @@
+Encoding.default_external = 'UTF-8'
+require 'csv'
+
+files = []
+
+dir = Dir.open(".") 
+dir.each do |file|
+	files << file
+end
+
+file_all = files.select { |x| x.include?("csv")}
+file_all.sort!
+
+
+
+array = []
+file_all.each_with_index do |file_name, index|
+	if index == 0
+	 	csv = CSV.read("#{file_name}", headers: false)
+		csv.each do |row|
+			array << row
+		end
+	else
+		csv = CSV.read("#{file_name}", headers: true)
+		csv.each do |row|
+			array << row
+		end
+	end
+end
+
+CSV.open("test.csv","w") do |test|
+    array.each do |data|
+    	test << data
+    end
+end
